07f8588f366f76b0c90c442a56c5bf9f9631c0db
zyuq <xiaobolee@zyqMacBook.local>
2013-06-25 09:16:44 +0800

    Initial Commit

---
+++ AngryBirds/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  AngryBirdsAppController.h
+//  AngryBirds
+//
+//  Created by zyuq on 13-6-25.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ AngryBirds/ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AngryBirds
+//
+//  Created by zyuq on 13-6-25.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
